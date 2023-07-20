10287547730a1534fadb5af5130836e278fd8760
naresh <nareshvelamakuru@gmail.com>
2023-07-20 23:02:10 +0530

    frontend sh file updated

--- frontend.sh
+++ frontend.sh
@@ -1,26 +1,16 @@
 #sudo -s
-#sudo set-hostname frontend
-#yum install nginx -y
-#
-#cp -f roboshop.conf /etc/nginx/default.d/
-#
-#systemctl enable nginx
-#systemctl restart nginx
-#
-#rm -rf /usr/share/nginx/html/*
-#curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-#cd /usr/share/nginx/html
-#unzip /tmp/frontend.zip
-#
-#systemctl restart nginx
-
+sudo set-hostname frontend
 yum install nginx -y
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+cp -f roboshop.conf /etc/nginx/default.d/
+
+systemctl enable nginx
+systemctl restart nginx
 
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-systemctl enable nginx
 systemctl restart nginx
+
