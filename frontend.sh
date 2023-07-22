47406bc6e1734a2c0c289d7abbc6e08f08ce154b
Ironman <nareshvelamakuru@gmail.com>
2023-07-22 14:26:48 +0530

    frontend.sh & monogdb.sh updated

--- frontend.sh
+++ frontend.sh
@@ -5,21 +5,25 @@ RED='\e[31m'
 RESET='\e[0m'
 
 
-echo -e "${RED}>>>>>>>>>>>>> Installing nginx <<<<<<<<<<${RESET}";
+echo -e "${RED}>>>>>>>>>>>>> Installing nginx <<<<<<<<<<${RESET}"
 yum install nginx -y
 cp -f roboshop.conf /etc/nginx/default.d/
+
 
 echo -e "${RED}>>>>>>>>>>> starting nginx <<<<<<<<<<<<<<"
 systemctl enable nginx
 systemctl restart nginx
 
+
 echo -e "${RED}>>>>>>>>>>>>> Removing nginx file <<<<<<<<<<<<<<<<<<<${RESET}"
 rm -rf /usr/share/nginx/html/*
+
 
 echo -e "${RED}>>>>>>>>>>>>> downloading Frontend code <<<<<<<<<<<<<<<<<<<${RESET}"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
+echo -e "${RED}>>>>>>>>>>>>> restarting nginx <<<<<<<<<<<<<<<<<<<${RESET}"
 systemctl restart nginx
 
--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,19 @@
-sudo set-hostname mongodb
+RED='\e[31m'
+RESET='\e[0m'
 
 
+echo -e "${RED}>>>>>>>>>>>>> removing repo files <<<<<<<<<<${RESET}"
+rm -rf /etc/yum.repos.d/mongo.repo
+
+echo -e "${RED}>>>>>>>>>>>>> copying new repo files <<<<<<<<<<${RESET}"
 cp -f mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "${RED}>>>>>>>>>>>>> Installing mongod <<<<<<<<<<${RESET}"
 yum install mongodb-org -y
 
+echo -e "${RED}>>>>>>>>>>>>> enabling mongod <<<<<<<<<<${RESET}"
 systemctl enable mongod
 systemctl start mongod
 
+echo -e "${RED}>>>>>>>>>>>>> restarting mongod <<<<<<<<<<${RESET}"
 systemctl restart mongod
