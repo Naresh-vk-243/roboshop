309eaad18dc125e7011f994e9fbf8e26efdd0f26
naresh <nareshvelamakuru@gmail.com>
2023-07-25 16:21:45 +0530

    comments are update in some files

--- redis.sh
+++ redis.sh
@@ -4,8 +4,10 @@ End="\e[0m"
 echo  -e "${Red} <<<<<<<<<<<<<< Downloading the Redis rpm file which contains all versions >>>>>>>>>>>>>>${End}"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 
-echo  -e "${Red} <<<<<<<<<<<<<< Enabling the 6.2 veriosn from rpm file >>>>>>>>>>>>>>${End}"
+#echo  -e "${Red} <<<<<<<<<<<<<< Enabling the 6.2 veriosn from rpm file >>>>>>>>>>>>>>${End}"
+echo -e "${RED}>>>>>>>>>>>>> Installing nginx <<<<<<<<<<${RESET}"
 yum module enable redis:remi-6.2 -y
+
 
 
 echo  -e "${Red} <<<<<<<<<<<<<< installing the redis >>>>>>>>>>>>>>${End}"
