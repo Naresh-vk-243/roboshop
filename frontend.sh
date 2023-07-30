112021150d98728447823cfb9dc8d364bb5b7c71
Ironman <nareshvelamakuru@gmail.com>
2023-07-30 19:10:21 +0530

    commonFuncs.sh file updates

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,36 @@
-component=frontend
+log=/tmp/roboshop.log
+
+component=nginx
 source commonFuncs.sh
 
-echo -e "${RED}>>>>>>>>>>>>> Installing nginx <<<<<<<<<<${RESET}"
-yum install nginx -y&>>${log}
-rm -rf /etc/nginx/default.d/roboshop.conf
-cp -f roboshop.conf /etc/nginx/default.d/&>>${log}
 
-starting_enabling_without_daemon
-
-echo -e "${RED}>>>>>>>>>>>>> Removing nginx file <<<<<<<<<<<<<<<<<<<${RESET}"
-rm -rf /usr/share/nginx/html/*&>>${log}
+echo -e "\e[36m>>>>>>>>>>>>  Install Nginx   <<<<<<<<<<<<\e[0m"
+yum install nginx -y &>>${log}
+func_exit_status
 
 
-starting_enabling_without_daemon
+echo -e "\e[36m>>>>>>>>>>>>  Copy RoboShop Configuration  <<<<<<<<<<<<\e[0m"
+cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
+func_exit_status
+
+
+echo -e "\e[36m>>>>>>>>>>>>  Clean Old content  <<<<<<<<<<<<\e[0m"
+rm -rf /usr/share/nginx/html/* &>>${log}
+func_exit_status
+
+
+echo -e "\e[36m>>>>>>>>>>>>  Download Application Content   <<<<<<<<<<<<\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
+func_exit_status
+
+cd /usr/share/nginx/html
+
+echo -e "\e[36m>>>>>>>>>>>>  Extract Application Content  <<<<<<<<<<<<\e[0m"
+unzip /tmp/frontend.zip &>>${log}
+func_exit_status
+
+
+echo -e "\e[36m>>>>>>>>>>>>  Start Nginx Service  <<<<<<<<<<<<\e[0m"
+systemctl enable nginx &>>${log}
+systemctl restart nginx &>>${log}
+func_exit_status
